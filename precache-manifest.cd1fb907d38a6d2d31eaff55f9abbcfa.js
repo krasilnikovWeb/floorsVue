2deb59d09a09455533d0495db63ed220366db034
krasilnikovWeb <antoniys009@gmail.com>
2023-06-01 15:19:33 +0700

    build

---
+++ precache-manifest.cd1fb907d38a6d2d31eaff55f9abbcfa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ab7c9da46be0453da5",
+    "url": "/floors/css/app.27e930e3.css"
+  },
+  {
+    "revision": "89709714ef1e9b48660b",
+    "url": "/floors/css/chunk-0e1f3180.9acb1e31.css"
+  },
+  {
+    "revision": "55c51ee28d14283904ba",
+    "url": "/floors/css/chunk-vendors.9e4a5ece.css"
+  },
+  {
+    "revision": "39053032bd24ae91816680a0cc6d684d",
+    "url": "/floors/index.html"
+  },
+  {
+    "revision": "16ab7c9da46be0453da5",
+    "url": "/floors/js/app.2717612a.js"
+  },
+  {
+    "revision": "89709714ef1e9b48660b",
+    "url": "/floors/js/chunk-0e1f3180.52c28ce2.js"
+  },
+  {
+    "revision": "287037bb3ab0ec4315fc",
+    "url": "/floors/js/chunk-2d0b1fc7.90563c67.js"
+  },
+  {
+    "revision": "55c51ee28d14283904ba",
+    "url": "/floors/js/chunk-vendors.34b3099b.js"
+  },
+  {
+    "revision": "01ebd8cb64c39a81a90d1f39afe774b8",
+    "url": "/floors/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/floors/robots.txt"
+  }
+]);
